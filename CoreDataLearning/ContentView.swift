08d57da7848edf9619bd65610d3274ac267bd87f
Jacob Kelly <jskelly2021@gmail.com>
2024-08-31 15:25:13 -0500

    Initial Commit

---
+++ CoreDataLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreDataLearning
+//
+//  Created by Jacob Kelly on 8/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreDataLearning/CoreDataLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataLearningApp.swift
+//  CoreDataLearning
+//
+//  Created by Jacob Kelly on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
